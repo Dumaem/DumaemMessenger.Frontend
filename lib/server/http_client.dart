9b535c9619c4b9318519bedf2b2dc7cc2ad01026
riazonbin <riazonbin@mail.ru>
2023-05-05 12:24:18 +0300

    changed base url for dio

--- lib/server/http_client.dart
+++ lib/server/http_client.dart
@@ -3,7 +3,8 @@ import 'package:dio/dio.dart';
 class HttpClient {
   static late Dio dio;
 
-  static BaseOptions options = BaseOptions(baseUrl: 'затычка');
+  static BaseOptions options =
+      BaseOptions(baseUrl: 'https://localhost:7213/api/');
 
   static void initializeStaticDio() {
     dio = Dio(options);
