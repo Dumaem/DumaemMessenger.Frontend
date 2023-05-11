8b260df99980a6dd987850fdccbdc8fd3ecab538
dobriy_yashcher <golovinsergey17@gmail.com>
2023-05-11 14:49:20 +0300

    added chat info page and some images

---
+++ lib/pages/chat_info_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChatInfo extends StatefulWidget {
+  const ChatInfo({Key? key}) : super(key: key);
+
+  @override
+  State<ChatInfo> createState() => _ChatInfoState();
+}
+
+class _ChatInfoState extends State<ChatInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
