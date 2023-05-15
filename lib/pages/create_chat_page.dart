7e1636aed5bf7785aee9584197551ea1e5b7b0db
dobriy_yashcher <golovinsergey17@gmail.com>
2023-05-15 16:27:13 +0300

    start commit

--- lib/pages/create_chat_page.dart
+++ lib/pages/create_chat_page.dart
@@ -10,7 +10,17 @@ class CreateChatPage extends StatefulWidget {
 class _CreateChatPageState extends State<CreateChatPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.popAndPushNamed(context, '/home');
+          },
+        ),
+      )
+    );
   }
 }
 
