e2099aac179b2a939125353a3374e94ef6b12116
Flictivity <bulat_sa1@mail.ru>
2023-05-16 22:59:53 +0300

    working at chat messages loading

--- lib/pages/chat.dart
+++ lib/pages/chat.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
@@ -38,8 +37,25 @@ class _ChatPageState extends State<ChatPage> {
           messages: _filter_messages,
           onSendPressed: _handleSendPressed,
           user: _user,
+          onEndReached: _handleEndReached,
         ),
       );
+
+  Future<void> _handleEndReached() async {
+    final messages = data
+        .map(
+          (e) => types.TextMessage(
+            author: _user,
+            id: e['_id'] as String,
+            text: e['name'] as String,
+          ),
+        )
+        .toList();
+    setState(() {
+      _messages = [..._messages, ...messages];
+      _page = _page + 1;
+    });
+  }
 
   void _addMessage(types.Message message) {
     setState(() {
@@ -127,22 +143,6 @@ class _ChatPageState extends State<ChatPage> {
   }
 }
 
-class Message {
-  int? id;
-  String? sender;
-  String? content;
-  Message({this.id, this.sender, this.content});
-}
-
-List<Message> messagesList = [
-  Message(id: 1, sender: 'Фермер', content: 'Привет!'),
-  Message(id: 2, sender: 'Копатель', content: 'Привет!'),
-  Message(id: 3, sender: 'Космонавт', content: 'Привет!'),
-  Message(id: 4, sender: 'Майнер', content: 'Привет!'),
-  Message(id: 5, sender: 'Ринат', content: 'Привет!'),
-  Message(id: 6, sender: 'Камиль', content: 'Привет!'),
-];
-
 class InputWidget extends StatelessWidget {
   const InputWidget({
     super.key,
@@ -165,8 +165,8 @@ class InputWidget extends StatelessWidget {
                       offset: Offset(0, 2), blurRadius: 7, color: Colors.grey)
                 ],
               ),
-              child: Row(
-                children: const [
+              child: const Row(
+                children: [
                   Expanded(
                     child: TextField(
                       decoration: InputDecoration(
--- lib/server/chat/chat_service.dart
+++ lib/server/chat/chat_service.dart
@@ -1,22 +1,37 @@
+import 'package:dumaem_messenger/models/message_context.dart';
+
 import '../../models/chat_list_model.dart';
 import '../dio_http_client.dart';
 import '../global_variables.dart';
 
-class ChatService{
-  Future<List<ChatListModel>> getChatsView() async
-  {
-    List<ChatListModel> data = List.empty(growable: true); 
+class ChatService {
+  Future<List<ChatListModel>> getChatsView() async {
+    List<ChatListModel> data = List.empty(growable: true);
 
     int userId = int.parse(await storage.read(key: userKey) as String);
-    Map<String, int> queryParameters = {
-      "id": userId
-    };
-    var response = await DioHttpClient.dio.get('Chat/get-user-chats-by-id', queryParameters: queryParameters);
+    Map<String, int> queryParameters = {"id": userId};
+    var response = await DioHttpClient.dio
+        .get('Chat/get-user-chats-by-id', queryParameters: queryParameters);
 
- 
-    for(var jsonData in response.data)
-    {
+    for (var jsonData in response.data) {
       data.add(ChatListModel.fromJson(jsonData));
+    }
+    return data;
+  }
+
+  Future<List<MessageContext>> getChatMessages(
+      String chatName, int count, int page) async {
+    List<MessageContext> data = List.empty(growable: true);
+    Map<String, dynamic> queryParameters = {
+      'chatName': chatName,
+      'count':count,
+      'page':page
+    };
+    var response = await DioHttpClient.dio
+        .get('Message', queryParameters: queryParameters);
+
+    for (var jsonData in response.data) {
+      data.add(MessageContext.fromJson(jsonData));
     }
     return data;
   }
