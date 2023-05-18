e38481dfb7d34cb9d2ec1592cc8b233300ecd3f0
dobriy_yashcher <golovinsergey17@gmail.com>
2023-05-18 20:39:06 +0300

    MESSENGER-14 добавил подгрузку информации о чате

--- lib/tabs/chat_participants_view.dart
+++ lib/tabs/chat_participants_view.dart
@@ -1,120 +1,74 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 
+import '../models/user_model.dart';
+import '../properties/chat_page_arguments.dart';
 import '../properties/config.dart';
+import '../server/user/user_service.dart';
 
-class ChatParticipantsView extends StatelessWidget {
-  const ChatParticipantsView({Key? key}) : super(key: key);
+class ChatParticipantsView extends StatefulWidget {
+  ChatParticipantsView({Key? key}) : super(key: key);
+
+  @override
+  State<ChatParticipantsView> createState() => _ChatParticipantsViewState();
+}
+
+class _ChatParticipantsViewState extends State<ChatParticipantsView> {
+  List<UserModel> usersList = List.empty(growable: true);
+
+  final _userService = UserService();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(
-        children: ParticipantsList.map(
-          (Participant) {
-            return Card(
-              child: ListTile(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(smallBorderRadius),
-                ),
-                leading: CircleAvatar(
-                  child: Text(Participant.name![0].toUpperCase()),
-                ),
-                trailing: IconButton(
-                  onPressed: RemoveParticipant,
-                  icon: const Icon(Icons.remove_circle_outlined),
-                ),
-                title: Text(Participant.name!,
-                    style: const TextStyle(fontWeight: FontWeight.bold)),
-                // onTap: () {
-                //   Navigator.pushNamed(context, '/Participant',
-                //       arguments: ScreenArguments(Participant.id));
-                // },
-              ),
-            );
-          },
-        ).toList(),
-      ),
+    Future<List<UserModel>> _getUsers() async {
+      if (usersList.isEmpty) {
+        usersList = await _userService.getChatMembers(
+            (ModalRoute.of(context)!.settings.arguments
+            as ScreenArguments)
+                .chatGuid as String);
+      }
+      return usersList;
+    }
+
+    return FutureBuilder<List<UserModel>>(
+      future: _getUsers(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return const CircularProgressIndicator();
+        } else {
+          if (usersList.isEmpty) {
+            usersList = snapshot.data as List<UserModel>;
+          }
+          return Scaffold(
+            body: ListView.builder(
+              itemCount: usersList.length,
+              itemBuilder: (context, index) {
+                return InkWell(
+                  child: Card(
+                    child: ListTile(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(smallBorderRadius),
+                      ),
+                      leading: CircleAvatar(
+                        child: Text(usersList[index].name![0].toUpperCase()),
+                      ),
+                      trailing: IconButton(
+                        onPressed: RemoveParticipant,
+                        icon: const Icon(Icons.remove_circle_outlined),
+                      ),
+                      title: Text(usersList[index].name!,
+                          style: const TextStyle(fontWeight: FontWeight.bold)),
+                    ),
+                  ),
+                );
+              },
+            ),
+          );
+        }
+      },
     );
   }
 
   void RemoveParticipant() {}
 }
-
-
-// test data
-class Participant {
-  int id;
-  String? name;
-
-  Participant({
-    required this.id,
-    this.name,
-  });
-}
-
-List<Participant> ParticipantsList = [
-  Participant(
-    id: 1,
-    name: 'Фермеры',
-  ),
-  Participant(
-    id: 2,
-    name: 'КТИТС',
-  ),
-  Participant(
-    id: 3,
-    name: 'думаем',
-  ),
-  Participant(
-    id: 4,
-    name: "Избранное",
-  ),
-  Participant(
-    id: 5,
-    name: 'Фермеры',
-  ),
-  Participant(
-    id: 6,
-    name: 'КТИТС',
-  ),
-  Participant(
-    id: 7,
-    name: 'думаем',
-  ),
-  Participant(
-    id: 8,
-    name: "Избранное",
-  ),
-  Participant(
-    id: 9,
-    name: 'Фермеры',
-  ),
-  Participant(
-    id: 10,
-    name: 'КТИТС',
-  ),
-  Participant(
-    id: 11,
-    name: 'думаем',
-  ),
-  Participant(
-    id: 12,
-    name: "Избранное",
-  ),
-  Participant(
-    id: 9,
-    name: 'Фермеры',
-  ),
-  Participant(
-    id: 10,
-    name: 'КТИТС',
-  ),
-  Participant(
-    id: 11,
-    name: 'думаем',
-  ),
-  Participant(
-    id: 12,
-    name: "Избранное",
-  ),
-];
